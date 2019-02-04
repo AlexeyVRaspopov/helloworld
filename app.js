7837f3317cc02255f21d679c7bcea0b97ba7f88a
alexras <aleks368@mail.ru>
2019-02-04 10:39:46 +0100

    add 22

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 app.get("/", function(request, response){
      
-    response.send("<h2>Hello World!</h2>");
+    response.send("<h2>Hello World22!</h2>");
 });
 app.listen(3000);
