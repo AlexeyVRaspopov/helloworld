0fde3fed05e9ab8c903a0d2c15e54411e2047a54
alexras <aleks368@mail.ru>
2019-03-12 12:57:14 +0100

    fix

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/", function(request, response){
+    response.send("<h2>Hello World!!</h2>");
+})
      
-<<<<<<< HEAD:src/app.js
-    response.send("<h2>Hello World!!</h2>");
-=======
-    response.send("<h2>Hello World!!</h2>");
->>>>>>> f6eb90d4db00993601522d4a9f8e4be21029e679:app.js
-});
 app.listen(3000);
