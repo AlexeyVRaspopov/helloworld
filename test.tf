858aeb6671244a3abb65fab45d046487b74c235e
AlexeyVRaspopov <40714943+AlexeyVRaspopov@users.noreply.github.com>
2020-02-26 18:15:53 +0300

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,7 +1,10 @@
 terraform {
-  backend "azurerm" {
-    storage_account_name = "DefaultEndpointsProtocol=https;AccountName=alexrasfiles;AccountKey=w1/1eVs1y123A1Jy0MHyzkFuLyvook7rvGbklOZbWZ3nhsV+/3YHl/rzyTBqZZ082+EHOYLpEKhuMzhTMVeD5Q==;EndpointSuffix=core.windows.net"
+  backend = "azurerm"
+  config = {
+    storage_account_name = "alexrasfiles"
     container_name       = "test"
+    key                  = "w1/1eVs1y123A1Jy0MHyzkFuLyvook7rvGbklOZbWZ3nhsV+/3YHl/rzyTBqZZ082+EHOYLpEKhuMzhTMVeD5Q=="
+    #sas_token            = run_cmd("${get_parent_terragrunt_dir()}/set-access-key.sh", "tfstatedv")
   }
 }
 resource "azurerm_resource_group" "example" {
