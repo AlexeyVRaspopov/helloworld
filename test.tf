17cff8afdf3d2fe7cf23a82f6b60f5b46ff2dfc9
AlexeyVRaspopov <40714943+AlexeyVRaspopov@users.noreply.github.com>
2020-02-26 18:37:41 +0300

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,5 +1,4 @@
 provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   version = "=2.0.0"
   features {}
 }
@@ -7,7 +6,6 @@ terraform {
   backend "azurerm" {
     storage_account_name = "alexrasfiles"
     container_name       = "test"
-  #  key                  = "w1/1eVs1y123A1Jy0MHyzkFuLyvook7rvGbklOZbWZ3nhsV+/3YHl/rzyTBqZZ082+EHOYLpEKhuMzhTMVeD5Q=="
   }
 }
 resource "azurerm_resource_group" "example" {
