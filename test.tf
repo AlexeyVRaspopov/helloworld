c88a9339ad4a42939b5f7b333a550b05de677948
AlexeyVRaspopov <40714943+AlexeyVRaspopov@users.noreply.github.com>
2020-02-26 18:17:19 +0300

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,6 +1,5 @@
 terraform {
-  backend = "azurerm"
-  config = {
+  backend = "azurerm" {
     storage_account_name = "alexrasfiles"
     container_name       = "test"
     key                  = "w1/1eVs1y123A1Jy0MHyzkFuLyvook7rvGbklOZbWZ3nhsV+/3YHl/rzyTBqZZ082+EHOYLpEKhuMzhTMVeD5Q=="
